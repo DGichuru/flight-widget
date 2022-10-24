0f72a7aa11402f7e49431ee6cc1a782649c0caa1
DGichuru <dangichuru77@gmail.com>
2022-10-24 11:06:48 +0300

    updated table

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,6 +1,18 @@
-const TableRow  = () => {
+import TableCell from "./TableCell";
+
+
+const TableRow  = ({flight }) => {
+
+  const  words  = Object.values(flight )
     return (
-      <></>
+      <tr>
+        <td>✈︎</td>
+        {words?.map((word , _index) => (
+          <TableCell key={_index} word = {word} />
+        ))}
+      
+
+      </tr>
   
     );
   }
